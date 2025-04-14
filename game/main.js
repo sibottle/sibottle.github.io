3ab6e8101c94884d8d909785272aa42e4e393a79
sibottle <stage0728@gmail.com>
2025-04-14 15:55:45 +0900

    ohh

--- game/main.js
+++ game/main.js
@@ -1,5 +1,47 @@
 import { FRUITS } from "./fruits.js";
 
+const loadTexture = async () => {
+
+    const textureList = [
+    'image/00_cherry.png',
+    'image/01_strawberry.png',
+    'image/02_grape.png',
+    'image/03_gyool.png',
+    'image/04_orange.png',
+    'image/05_apple.png',
+    'image/06_pear.png',
+    'image/07_peach.png',
+    'image/08_pineapple.png',
+    'image/09_melon.png',
+    'image/10_watermelon.png',
+    ]
+    
+    const load = textureUrl => {
+    const reader = new FileReader()
+    
+    return new Promise( resolve => {
+    reader.onloadend = ev => {
+    resolve(ev.target.result)
+    }
+    fetch(textureUrl).then( res => {
+    res.blob().then( blob => {
+    reader.readAsDataURL(blob)
+    })
+    })
+    })
+    }
+    
+    const ret = {}
+    
+    for ( let i = 0; i < textureList.length; i++ ) {
+    ret[textureList[i]] = await load(`${textureList[i]}`)
+    }
+    
+    return ret
+    }
+    
+    const textureMap = await loadTexture()
+
 var Engine = Matter.Engine,
     Render = Matter.Render,
     Runner = Matter.Runner,
@@ -80,7 +122,6 @@ Events.on(engine, "collisionStart", (event) => {
     event.pairs.forEach((c) => {
         if (c.bodyA.index == c.bodyB.index) {
             const index = c.bodyA.index;
-            World.remove(world,[c.bodyA,c.bodyB]);
             const newFruit = FRUITS[index+1];
             const newBody = Bodies.circle(
                 c.collision.supports[0].x,
@@ -92,6 +133,17 @@ Events.on(engine, "collisionStart", (event) => {
                 }
             )
 
+            if (index == 9) {
+                setTimeout(() => {
+                    alert("Congrats! You have made a Watermelon!")
+                    disableAction = true
+                }, 1000)
+            }
+
+            Body.applyForce(newBody,c.collision.supports[0], c.bodyA.force)
+
+            World.remove(world,[c.bodyA,c.bodyB]);
+
             World.add(world, newBody);
         }
         if ((c.bodyA.name === "topLine" || c.bodyB.name === "topLine") && !disableAction) {
@@ -101,7 +153,7 @@ Events.on(engine, "collisionStart", (event) => {
     })
 });
 
-addFruit(Math.floor(Math.random()*10))
+addFruit(Math.floor(Math.random()*5))
 
 let keys = {
     "a": false,
@@ -140,7 +192,7 @@ function placeFruit() {
     currentBody.isSleeping = false;
     setTimeout(() => {
         disableAction = false;
-        addFruit(Math.floor(Math.random()*10))
+        addFruit(Math.floor(Math.random()*5))
     }, 1000)
 }
 
