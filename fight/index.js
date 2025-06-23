622218fb2cfbf8419ddb54f6d0a3833b8d2bc147
null <null>
2025-06-23 14:49:43 +0900

    Sprite

--- fight/index.js
+++ fight/index.js
@@ -5,3 +5,32 @@ canvas.width = 1024;
 canvas.height = 576;
 
 c.fillRect(0,0,canvas.width,canvas.height);
+
+class Sprite{
+    constructor( {position, velocity, width, height} ) {
+        this.position = position;
+        this.velocity = velocity
+        this.width = width;
+        this.height = height
+    }
+
+    draw() {
+        c.fillStyle = "red";
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+}
+
+const player = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    velocity:{
+        x: 0,
+        y: 0
+    },
+    width: 50,
+    height:120
+})
+
+player.draw()
